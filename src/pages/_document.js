7150b249ad6c0a4e43e01df017f420d0945828bd
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2020-07-18 16:26:14 +0800

    Fix FUOC again

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,29 +1,38 @@
+import React from 'react';
 import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
-import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
-    const styleTags = sheet.getStyleElement();
+    const originalRenderPage = ctx.renderPage;
 
-    return {
-      ...page,
-      styleTags,
-    };
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width" />
-          {this.props.styleTags}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
